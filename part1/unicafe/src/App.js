27d7fd8baf36e43dad8a46703b2a56325880e37f
nuKaross <127345332+nuKaross@users.noreply.github.com>
2023-04-24 21:53:28 +0200

    unicafe step3 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 
+// Feedback statistics
 const Stats = ({ allClicks }) => {
-  let resultsPNB = [0, 0, 0];
-
+  const resultsPNB = [0, 0, 0];
+  //Counts each vote and puts it as a number
+  //Index[0]= P, [1]=N, [2]=B
   allClicks.forEach((element) => {
     if (element === "P") {
       return resultsPNB[0]++;
@@ -12,15 +14,30 @@ const Stats = ({ allClicks }) => {
     return resultsPNB[2]++;
   });
 
+  //All counts sum:
+  const sum = resultsPNB.reduce((total, num) => total + num, 0);
+  //Positive feedback percentage:
+  const positiveFeedBack = (resultsPNB[0] / sum) * 100;
+  //Returns avg feedback as if: good = 1, bad = -1:
+  const avgFeedBack =
+    (resultsPNB[0] - resultsPNB[2]) / (resultsPNB[0] + resultsPNB[2]);
+
+  //What app renders:
   return (
     <div>
       <p>Positive: {resultsPNB[0]}</p>
       <p>Neutral: {resultsPNB[1]}</p>
       <p>Bad: {resultsPNB[2]}</p>
+      <p>All results: {sum} </p>
+      <p>
+        Positive feedback: {isNaN(positiveFeedBack) ? 0 : positiveFeedBack}%
+      </p>
+      <p>Average feedback: {isNaN(avgFeedBack) ? 0 : avgFeedBack} </p>
     </div>
   );
 };
 
+//Reusable generic button
 const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
