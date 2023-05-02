ee1a48141546eb4e241d8ac5321bec3700bfbf24
nuKaross <127345332+nuKaross@users.noreply.github.com>
2023-05-02 16:13:39 +0200

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,62 @@
 import { useState } from "react"
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
 
+  const AddNew = (event) => {
+    event.preventDefault()
+    const newObject = {
+      name: newName,
+      id: newName,
+      number: newNumber,
+    }
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} Already Exists`)
+    } else {
+      setPersons(persons.concat(newObject))
+      setNewName("")
+      setNewNumber("")
+    }
+  }
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          number: <input value={newNumber} onChange={handleNumChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={AddNew}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ol>
+        {persons.map((person) => (
+          <li key={person.id}>
+            <b>Name: </b>
+            {person.name}
+            <b> Num: </b>
+            {person.number}
+          </li>
+        ))}
+      </ol>
     </div>
   )
 }
